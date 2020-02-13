aa551e54cc73277b995cac4d1bf16098ba7c9987
shalva-an <s.halvaa26@gmail.com>
2020-02-13 10:00:32 -0500

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  get 'followings/show'
-  get '/users/signup' => 'users#signup'
-  get '/signin' => 'sessions#signin'
-  get '/signout' => 'sessions#signout'
+  get '/users/signup', to: 'users#signup'
+  get '/signin', to: 'sessions#signin'
+  get '/signout', to: 'sessions#signout'
   root 'sessions#signin'
   
   resources :sessions, only: :create
+  resources :reactions, only: :create
   resources :users, except: :new
   resources :pictures
   resources :tags
   resources :comments
+  resources :followings
 
 end
