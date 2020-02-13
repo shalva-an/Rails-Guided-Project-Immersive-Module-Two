ed80516b5bb760f3ab6724f8068fd695ff83725a
shalva-an <s.halvaa26@gmail.com>
2020-02-13 14:23:05 -0500

    'cookie' bug fixed
    app > controllers > application_controller.rb was missing one word

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
 
     def current_user
         if session[:user_id]
-            @current_user = User.find[:user_id]
+            @current_user = User.find(session[:user_id])
         else
             false
         end
